663732814f20e99036bdf6a0eb92dd88302ef69f
leosteil <lsteil@inf.ufsm.br>
2019-07-05 00:04:15 -0300

    webpack and babel configurations
    webpack and babel configurations

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './teste';
 
-console.log(`I imported ${x} from sdfsdf module`);
+console.log(`I imported ${x} from sdfsdf652645445564 module`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	//entry ponit
-	entry: './src/js/index.js',
+	entry: ['@babel/polyfill', './src/js/index.js'],
 
 	//output where to salve the bundle
 	output: {
-		path: path.resolve(__dirname, 'dist/js'),
-		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js',
 	},
 
 	devServer: {
 		contentBase: './dist'
-	}
+	},
+
+	//plugins
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	],
 
 	//loaders
 
-	//plugins
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
+
+
 };
